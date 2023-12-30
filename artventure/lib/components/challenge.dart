c72f47a3352b66eaa8fde607b5893a63b226a499
ntua-el20827 <el20827@mail.ntua.gr>
2023-12-30 22:23:37 +0200

    problem_with_photos

--- artventure/lib/components/challenge.dart
+++ artventure/lib/components/challenge.dart
@@ -1,112 +1,117 @@
 import 'package:flutter/material.dart';
 
+enum ChallengeState {
+  open,
+  inProgress,
+  done,
+}
+
 class Challenge {
   final String name;
   final String info;
   final int points;
-  final ImageProvider<Object> imageProvider;
+  final String image;
+  final List<String> categories;
+  ChallengeState state;
 
   Challenge({
     required this.name,
     required this.info,
     required this.points,
-    ImageProvider<Object>? imageProvider,
-  }) : imageProvider =
-            imageProvider ?? AssetImage('assets/images/image_not_found.png');
+    required this.categories,
+    required this.state,
+    required this.image,
+  });
 
-  Widget buildBigCard(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      padding: EdgeInsets.all(16),
+  Widget buildSmallChallengeCard() {
+    return Card(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: 200,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
-              ),
-            ),
+          Image.network(
+            image.isNotEmpty ? image : 'assets/images/image_not_found.png',
+            fit: BoxFit.cover,
+            height: 50.0,
           ),
-          SizedBox(height: 16),
-          Text(
-            name,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-          SizedBox(height: 8),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Text(
-                info,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _buildDiamondPoints(context),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Handle the action when the user presses the "Done" button
-                    },
-                    child: Text("Done"),
-                  ),
-                  SizedBox(width: 8),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Handle the action when the user presses the "Remove" button
-                    },
-                    child: Text("Remove"),
-                  ),
-                ],
-              ),
-            ],
+          ListTile(
+            title: Text(name),
+            subtitle: Text(info),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildDiamondPoints(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.blue, // Change the color as needed
-      ),
-      child: Center(
-        child: Text(
-          points.toString(),
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
+  Widget buildMediumChallengeCard() {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(
+            image.isNotEmpty ? image : 'assets/images/image_not_found.png',
+            fit: BoxFit.cover,
+            height: 75.0,
           ),
-        ),
+          ListTile(
+            title: Text(name),
+            subtitle: Text(info),
+          ),
+        ],
       ),
-      width: 40,
-      height: 40,
+    );
+  }
+
+  Widget buildLargeChallengeCard({
+    required Function() onButton1Pressed,
+    required Function() onButton2Pressed,
+  }) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(
+            image.isNotEmpty ? image : 'assets/images/image_not_found.png',
+            fit: BoxFit.cover,
+            height: 100.0,
+          ),
+          ListTile(
+            title: Text(name),
+            subtitle: Text(info),
+          ),
+          if (state == ChallengeState.open)
+            ButtonBar(
+              children: [
+                TextButton(
+                  onPressed: onButton1Pressed,
+                  child: Text('Start'),
+                ),
+              ],
+            ),
+          if (state == ChallengeState.inProgress)
+            ButtonBar(
+              children: [
+                TextButton(
+                  onPressed: onButton1Pressed,
+                  child: Text('Continue'),
+                ),
+              ],
+            ),
+          if (state == ChallengeState.done)
+            ButtonBar(
+              children: [
+                TextButton(
+                  onPressed: onButton1Pressed,
+                  child: Text('View Results'),
+                ),
+              ],
+            ),
+          ButtonBar(
+            children: [
+              TextButton(
+                onPressed: onButton2Pressed,
+                child: Text('Details'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- artventure/lib/pages/profile_page.dart
+++ artventure/lib/pages/profile_page.dart
@@ -1,101 +1,136 @@
+import 'package:artventure/components/challenge.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterflow_ui/flutterflow_ui.dart';
-import '../components/challenge.dart';
+
+class Profile {
+  final String name;
+  final String favoriteArt;
+  final String personality;
+  final int points;
+  final String
+      avatar; // You might replace this with the actual user avatar data from the database
+
+  Profile({
+    required this.name,
+    required this.favoriteArt,
+    required this.personality,
+    required this.points,
+    required this.avatar,
+  });
+}
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
-
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  // Sample user data (replace with data from your database)
-  String userName = "John Doe";
-  String favoriteArt = "Abstract Painting";
-  String personalityInfo = "Creative and adventurous";
-  int userPoints = 100; // Replace with actual user points from the database
+  // Sample user profile
+  Profile userProfile = Profile(
+    name: 'John Doe',
+    favoriteArt: 'Impressionism',
+    personality: 'Art Enthusiast',
+    points: 150,
+    avatar: 'path_to_avatar_image', // Replace with actual avatar data
+  );
 
-  // Sample challenges data (replace with data from your database)
+  // Sample list of challenges
   List<Challenge> challenges = [
     Challenge(
-      name: "Visit an Art Gallery",
-      info: "Visiting an art gallery is very good",
-      points: 20,
-    ),
+        name: 'Challenge 1',
+        info: 'Description for Challenge 1',
+        points: 20,
+        categories: ['Category 1'],
+        state: ChallengeState.inProgress,
+        image:
+            'https://en.wikipedia.org/wiki/Art#/media/File:Art-portrait-collage_2.jpg' // Replace with actual image data
+        ),
     Challenge(
-      name: "Create a Digital Artwork",
-      info: "Creating an Artwork is very good",
-      points: 30,
-    ),
+        name: 'Challenge 2',
+        info: 'Description for Challenge 2',
+        points: 30,
+        categories: ['Category 2'],
+        state: ChallengeState.done,
+        image:
+            'https://en.wikipedia.org/wiki/Art#/media/File:Art-portrait-collage_2.jpg' // Replace with actual image data
+        ),
+    // Add more challenges as needed
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: FlutterFlowTheme.of(context).info,
-        title: Align(
-          alignment: const AlignmentDirectional(0, 0),
-          child: Text(
-            'ArtVenture',
-            style: FlutterFlowTheme.of(context).titleLarge.override(
-                  fontFamily: 'MonteCarlo',
-                  fontSize: 30,
-                ),
-          ),
-        ),
-        actions: [],
-        centerTitle: false,
-        elevation: 2,
+        title: Text('ArtVenture'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          // Profile Info Segment
-          Container(
-            padding: EdgeInsets.all(16.0),
-            color: Colors.blue,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // Display user info
-                Text("Name: $userName"),
-                Text("Favorite Art: $favoriteArt"),
-                Text("Personality: $personalityInfo"),
-                Text("Points: $userPoints"),
-                // Display user avatar (replace with your avatar logic)
-                const CircleAvatar(
-                  backgroundImage:
-                      AssetImage('assets/images/image_not_found.png'),
-                  radius: 50,
-                ),
-              ],
-            ),
-          ),
-          // Challenges Segment
-          Expanded(
-            child: ListView.builder(
-              itemCount: challenges.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      _showChallengeDialog(challenges[index]);
-                    },
-                    child: challenges[index].buildBigCard(context),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Profile Info Segment
+            Container(
+              padding: EdgeInsets.all(16.0),
+              color: Colors.blue, // Add your desired color
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Profile Info',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 16.0),
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage(userProfile.avatar),
                   ),
-                );
-              },
+                  SizedBox(height: 16.0),
+                  Text('Name: ${userProfile.name}'),
+                  Text('Favorite Art: ${userProfile.favoriteArt}'),
+                  Text('Personality: ${userProfile.personality}'),
+                  Text('Points: ${userProfile.points}'),
+                ],
+              ),
             ),
-          ),
-        ],
+
+            // Challenges Segment
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Challenges',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 16.0),
+                  // In Progress Challenges
+                  Text('In Progress Challenges',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Column(
+                    children: challenges
+                        .where((challenge) =>
+                            challenge.state == ChallengeState.inProgress)
+                        .map((challenge) => userProfileCard(challenge))
+                        .toList(),
+                  ),
+                  SizedBox(height: 16.0),
+                  // Done Challenges
+                  Text('Done Challenges',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Column(
+                    children: challenges
+                        .where((challenge) =>
+                            challenge.state == ChallengeState.done)
+                        .map((challenge) => userProfileCard(challenge))
+                        .toList(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const [
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.account_circle),
             label: 'Profile',
           ),
           BottomNavigationBarItem(
@@ -108,22 +143,88 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ],
         onTap: (index) {
-          // Handle navigation based on the tapped index
-          // For example, you can use Navigator to push a new page
+          // Handle navigation to different pages
         },
       ),
     );
   }
 
-  // Function to show challenge details dialog
-  void _showChallengeDialog(Challenge challenge) {
+  Widget userProfileCard(Challenge challenge) {
+    return GestureDetector(
+      onTap: () {
+        showChallengePopup(challenge);
+      },
+      child: Card(
+        // Your card styling here
+        child: Column(
+          children: [
+            Image.asset(
+              challenge.image.isNotEmpty
+                  ? challenge.image
+                  : 'assets/images/default_image.png',
+              fit: BoxFit.cover,
+              height: 50.0,
+            ),
+            ListTile(
+              title: Text(challenge.name),
+              subtitle: Text(challenge.info),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showChallengePopup(Challenge challenge) {
     showDialog(
       context: context,
-      builder: (context) {
+      builder: (BuildContext context) {
         return AlertDialog(
-          content: challenge.buildBigCard(context),
+          title: Text(challenge.name),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                challenge.image.isNotEmpty
+                    ? challenge.image
+                    : 'assets/images/default_image.png',
+                fit: BoxFit.cover,
+                height: 100.0,
+              ),
+              Text(challenge.info),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                // Handle "Done" button press
+                setState(() {
+                  challenge.state = ChallengeState.done;
+                });
+                Navigator.pop(context);
+              },
+              child: Text('Done'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Handle "Remove" button press
+                setState(() {
+                  challenge.state = ChallengeState.open;
+                });
+                Navigator.pop(context);
+              },
+              child: Text('Remove'),
+            ),
+          ],
         );
       },
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ProfilePage(),
+  ));
 }
