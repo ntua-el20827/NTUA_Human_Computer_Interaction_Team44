cac243e024afba8f477b159a236e34aa059296ba
ntua-el20827 <el20827@mail.ntua.gr>
2024-01-20 19:50:20 +0200

    final

--- artventure/lib/main.dart
+++ artventure/lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:artventure/app_routes.dart';
 
+// This app is created for the class of Human Computer Interaction (NTUA-ECE) by the students:
+// 1. Ιωάννα Μπαμπαρούτση
+// 2. Μάνος Νεοφώτιστος
+// 3. Γιώργος Γεωργακόπουλος
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //final dbHelper = DatabaseHelper();
